c36e8aafd000a5f716b462e0784216bb8d91bf9f
xavier fabiani <xavier.fabiani@hotmail.fr>
2025-03-05 10:43:20 +0100

    done

--- app.py
+++ app.py
@@ -7,48 +7,23 @@ import pandas as pd
 # TaxiFareModel front
 '''
 
-st.markdown('''
-Remember that there are several ways to output content into your web page...
-
-Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
-''')
-
-'''
-## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
-
-1. Let's ask for:
-- date and time
-- pickup longitude
-- pickup latitude
-- dropoff longitude
-- dropoff latitude
-- passenger count
-'''
-
-'''
-## Once we have these, let's call our API in order to retrieve a prediction
-
-See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
-
-🤔 How could we call our API ? Off course... The `requests` package 💡
-'''
-
+st.markdown(
 url = 'https://taxifare.lewagon.ai/predict'
 
 if url == 'https://taxifare.lewagon.ai/predict':
 
     st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
 
-'''
+# '''
 
-2. Let's build a dictionary containing the parameters for our API...
+# 2. Let's build a dictionary containing the parameters for our API...
 
-3. Let's call our API using the `requests` package...
+# 3. Let's call our API using the `requests` package...
 
-4. Let's retrieve the prediction from the **JSON** returned by the API...
+# 4. Let's retrieve the prediction from the **JSON** returned by the API...
 
-## Finally, we can display the prediction to the user
-'''
+# ## Finally, we can display the prediction to the user
+# '''
 user_pickup_datetime = st.date_input('What is your pickup datetime ?')
 user_pickup_longitude = st.number_input('What is your pickup longitude (between -74.3 and -73.7) ?')
 user_pickup_latitude = st.number_input('What is your pickup latitude (between 40.5 and 40.9) ?')
@@ -67,14 +42,6 @@ params = {
     }
 
 
-# params_ok = {'pickup_datetime':datetime.strftime('%Y-%m-%dT%H:%M')
-#     # pd.Timestamp("2013-07-06 17:18:00", tz='UTC'),
-#              'pickup_longitude':-73.950655,
-#              'pickup_latitude':40.783282,
-#              'dropoff_longitude':-73.984365,
-#              'dropoff_latitude':40.769802,
-#              'passenger_count':1,
-#              }
 
 # st.write(requests.get(url, params_ok).json())
 
